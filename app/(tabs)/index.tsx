5d514dbd5100b0b69621b624536dc99388309e03
Maddisen Mohnsen <memohnsen@gmail.com>
2026-02-11 17:38:01 -0500

    working clcikc to visit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
+import { states } from '@/utils/states';
 import { BlurView } from 'expo-blur';
 import { GlassView } from 'expo-glass-effect';
 import { AppleMaps, GoogleMaps } from 'expo-maps';
+import { AppleMapsCircle } from 'expo-maps/build/apple/AppleMaps.types';
+import { useState } from 'react';
 import { Platform, Text, View } from "react-native";
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 
 export default function Index() {
+  const [visitedStates, setVisitedStates] = useState<String[]>([])
+
   const cameraPosition = {
     coordinates: {
       latitude: 39.833,
@@ -14,9 +19,26 @@ export default function Index() {
     zoom: 4
   }
 
-  const progress: number = 50
+  const markAsVisited = (tappedCircle: AppleMapsCircle) => {
+    const stateId = tappedCircle.id
+
+    if (!stateId) {
+      return
+    }
+
+    if (visitedStates.includes(stateId)) {
+      setVisitedStates(prevItems => 
+        prevItems.filter(item =>
+          item !== stateId
+        )
+      )
+    } else  {
+      setVisitedStates(prevItems => [...prevItems, stateId])
+    }
+  }
 
   if (Platform.OS === 'ios') {
+    // IOS VIEW
     return (
       <>
         <AppleMaps.View 
@@ -25,6 +47,26 @@ export default function Index() {
           uiSettings={{
             myLocationButtonEnabled: false
           }}
+          properties={{
+            isTrafficEnabled: false,
+            selectionEnabled: false,
+            isMyLocationEnabled: false
+          }}  
+          onCircleClick={(e) => {
+            markAsVisited(e)
+          }}
+          circles={
+            states.map((state) => {
+              return {
+                id: state.id,
+                center: { latitude: state.latitude, longitude: state.longitude },
+                radius: 60_000,
+                color: visitedStates.includes(state.id) ? "green" : "red",
+                lineColor: "black",
+                lineWidth: 1,
+              };
+            })
+          }
         />
         <GlassView
           style={{
@@ -37,39 +79,12 @@ export default function Index() {
             alignSelf: 'center',
           }}
         >
-          <View className='flex-row justify-between'>
-            <View className='flex gap-2'>
-              <Text className='text-text-primary font-bold'>Progress</Text>
-              <Text className='text-gray-400'>1 of 50 States Visited</Text>
-            </View>
-            <AnimatedCircularProgress
-              size={48}
-              width={4}
-              fill={progress}
-              tintColor="green"
-              backgroundColor="#3d5875"
-              rotation={360}
-            >
-              {
-                (progress: number) => (
-                  <Text className="text-text-primary text-xs">
-{Math.round(progress).toFixed(0)}
-%
-</Text>
-                )
-              }
-            </AnimatedCircularProgress>
-          </View>
-          <View className='flex-row justify-center items-center content-center mt-2'>
-              <View className='rounded bg-green-500 size-2 mr-1 mt-0.5' />
-              <Text className='text-gray-400 text-xs mr-4'>Visited</Text>
-              <View className='rounded bg-red-500 size-2 mr-1 mt-0.5' />
-              <Text className='text-gray-400 text-xs'>Not Visited</Text>
-          </View>
+          <StatesDetails />
         </GlassView>
       </>
     )
   } else {
+    // ANDROID VIEW
     return (
       <>
         <GoogleMaps.View 
@@ -92,37 +107,56 @@ export default function Index() {
             alignSelf: 'center',
           }}
         >
-          <View className='flex-row justify-between'>
-            <View className='flex gap-2'>
-              <Text className='text-white font-bold'>Progress</Text>
-              <Text className='text-gray-400'>1 of 50 States Visited</Text>
-            </View>
-            <AnimatedCircularProgress
-              size={48}
-              width={4}
-              fill={progress}
-              tintColor="green"
-              backgroundColor="#3d5875"
-              rotation={360}
-            >
-              {
-                (progress: number) => (
-                  <Text className="text-white text-xs">
-{Math.round(progress).toFixed(0)}
-%
-</Text>
-                )
-              }
-            </AnimatedCircularProgress>
-          </View>
-          <View className='flex-row justify-center items-center content-center mt-2'>
-              <View className='rounded bg-green-500 size-2 mr-1 mt-0.5' />
-              <Text className='text-gray-400 text-xs mr-4'>Visited</Text>
-              <View className='rounded bg-red-500 size-2 mr-1 mt-0.5' />
-              <Text className='text-gray-400 text-xs'>Not Visited</Text>
-          </View>
+          <StatesDetails />
         </BlurView>
       </>
     )
   }
+}
+
+// SHARED DETAIL SECTION
+
+function StatesDetails() {
+  const progress: number = 50
+
+  const visitedStatesCount = (): String => {
+    let count = 0
+    states.forEach(state => {
+      if (state.visited === true) {
+        count ++
+      }
+    });
+    return `${count} of 50 States Visited`
+  }
+
+  return(
+    <>
+      <View className='flex-row justify-between'>
+          <View className='flex gap-2'>
+            <Text className='text-text-primary font-bold'>Progress</Text>
+            <Text className='text-gray-400'>{visitedStatesCount()}</Text>
+          </View>
+          <AnimatedCircularProgress
+            size={48}
+            width={4}
+            fill={progress}
+            tintColor="green"
+            backgroundColor="#3d5875"
+            rotation={360}
+          >
+            {
+              (progress: number) => (
+                <Text className="text-text-primary text-xs">{Math.round(progress).toFixed(0)}%</Text>
+              )
+            }
+          </AnimatedCircularProgress>
+        </View>
+        <View className='flex-row justify-center items-center content-center mt-2'>
+            <View className='rounded bg-green-500 size-2 mr-1 mt-0.5' />
+            <Text className='text-gray-400 text-xs mr-4'>Visited</Text>
+            <View className='rounded bg-red-500 size-2 mr-1 mt-0.5' />
+            <Text className='text-gray-400 text-xs'>Not Visited</Text>
+        </View>
+    </>
+  )
 }
